8bbddd7d5ebe359a4370e4ae107efa0ffed1529c
Elvis Nuñez <elvisnunez@me.com>
2019-05-30 01:18:43 +0200

    Add view controller

--- Toyay/ViewController.swift
+++ Toyay/ViewController.swift
@@ -1,20 +1,22 @@
-//
-//  ViewController.swift
-//  Toyay
-//
-//  Created by Elvis Nuñez on 30/05/2019.
-//  Copyright © 2019 Elvis Nuñez. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        title = "30th May, 2019"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: String(describing: UITableViewCell.self))
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: UITableViewCell.self), for: indexPath)
+        cell.textLabel?.text = ":D"
+        return cell
+    }
 }
 
