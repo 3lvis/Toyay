d8ab984ccc6caa41dba4a2334928ccb25dc811ab
Elvis Nuñez <elvisnunez@me.com>
2019-06-06 11:22:34 +0200

    Add basic insertion

--- Toyay/ViewController.swift
+++ Toyay/ViewController.swift
@@ -1,16 +1,13 @@
 import UIKit
 
 class ViewController: UITableViewController {
-    let tasks = [
-        Task(title: "Do my homework ✍️"),
-        Task(title: "Do the laundry 👚"),
-        Task(title: "Make my first iPhone app 📱")
-    ]
+    var tasks = [Task]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addItem))
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -21,6 +18,28 @@ class ViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         cell.textLabel?.text = tasks[indexPath.row].title
         return cell
+    }
+
+    @objc func addItem() {
+        let alert = UIAlertController(title: "What do you need to get done?", message: nil, preferredStyle: .alert)
+
+        alert.addTextField(configurationHandler: nil)
+
+        let saveAction = UIAlertAction(title: "Save", style: .default, handler: { _ in
+            if let text = alert.textFields?.last?.text {
+                let indexPath = IndexPath(row: self.tasks.count, section: 0)
+                let task = Task(title: text)
+                self.tasks.append(task)
+                self.tableView.insertRows(at: [indexPath], with: .automatic)
+            }
+        })
+        alert.addAction(saveAction)
+
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+
+        alert.preferredAction = saveAction
+        present(alert, animated: true, completion: nil)
     }
 }
 
