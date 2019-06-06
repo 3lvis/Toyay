cc106b45d5c8e663c30724a6cd20baaa04d04fa8
Elvis Nuñez <elvisnunez@me.com>
2019-06-06 11:12:31 +0200

    Add UITableViewController with hardcoded cells

--- Toyay/ViewController.swift
+++ Toyay/ViewController.swift
@@ -1,12 +1,20 @@
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Step 2:
-        // Change this to `view.backgroundColor = .red`
-        view.backgroundColor = .white
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Hello WiT"
+        return cell
     }
 }
 
